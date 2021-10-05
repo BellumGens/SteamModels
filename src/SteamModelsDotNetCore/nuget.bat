643144fc26509630d5373d851c963d75ab434bab
Konstantin Dinev <kdinev@mail.bw.edu>
2021-10-05 11:59:42 +0300

    feat(*): leaving only one project

--- src/SteamModelsDotNetCore/nuget.bat
+++
@@ -1,4 +0,0 @@
-nuget pack .\SteamModelsDotNetCore.csproj
-nuget push .\SteamModelsDotNetCore.<version>.nupkg -Source https://www.nuget.org/api/v2/package
-
-nuget push .\bin\Release\SteamModelsDotNetCore.1.0.0.nupkg -Source https://www.nuget.org/api/v2/package
